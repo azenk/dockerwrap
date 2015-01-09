2048d8c2ea00dfb7391cb1db1cb81679db7c2602
Andrew Zenk <andrew@andrewzenk.com>
2015-01-08 20:52:04 -0600

    Removed dependency on bash

--- config_template_processor.sh
+++ config_template_processor.sh
@@ -1,18 +1,16 @@
-#!/bin/bash
-config_file_paths=$(cat $1)
-shift
+#!/bin/sh
+allowed_variables=$(cat /etc/allowed_variables)
+config_file_paths=$(cat /etc/config_manifest)
+
+sedscript=$(mktemp)
+for varname in ${allowed_variables} ; do
+	value=$(eval echo -n \$${varname} | sed 's/%/\\%/g')
+	echo "s%\${${varname}}%${value}%g" 
+done > ${sedscript}
 
 for file in "${config_file_paths}" ; do
-	tempfile=$(mktemp)
-	while read -r line ; do
-    		while [[ "$line" =~ (\$\{[a-zA-Z_][a-zA-Z_0-9]*\}) ]] ; do
-        		LHS=${BASH_REMATCH[1]}
-        		RHS="$(eval echo "\"$LHS\"")"
-        		line=${line//$LHS/$RHS}
-    		done
-    		echo "$line" >> ${tempfile}
-	done < ${file}
-	mv ${tempfile} ${file}
+	sed -f ${sedscript} -i ${file}
 done
+rm ${sedscript}
 
 exec "$@"
